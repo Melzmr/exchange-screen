f92e98d46cfa71f863a722d1cc018ba058f2850b
Roman Martynenko <rm.melz@gmail.com>
2020-04-05 23:54:20 +0300

    Updates

---
+++ precache-manifest.6620313db7afd0dde2e4f421e63d1cf9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015d87868a513674636921a118d0bb84",
+    "url": "/exchange-screen/index.html"
+  },
+  {
+    "revision": "79df5364c4eb886b23c6",
+    "url": "/exchange-screen/static/css/2.95733882.chunk.css"
+  },
+  {
+    "revision": "e9072d112fddf0483f3e",
+    "url": "/exchange-screen/static/css/main.fe7a7b08.chunk.css"
+  },
+  {
+    "revision": "79df5364c4eb886b23c6",
+    "url": "/exchange-screen/static/js/2.0daf4305.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/exchange-screen/static/js/2.0daf4305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9072d112fddf0483f3e",
+    "url": "/exchange-screen/static/js/main.a274a529.chunk.js"
+  },
+  {
+    "revision": "0fd78090e96ceb097699",
+    "url": "/exchange-screen/static/js/runtime-main.d10fcf02.js"
+  },
+  {
+    "revision": "9af8704c36d61c66ce6896f21cb54a21",
+    "url": "/exchange-screen/static/media/background.9af8704c.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/exchange-screen/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/exchange-screen/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/exchange-screen/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/exchange-screen/static/media/slick.f97e3bbf.svg"
+  }
+]);
